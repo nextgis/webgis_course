6cc8eb6c8e923193e1db2a7f37edc40dd3c2dce7
drnextgis <rykovd@gmail.com>
2015-11-28 11:29:22 +0600

    update license

--- license.js
+++ license.js
@@ -1,8 +1,7 @@
 (function(){
-    var license = '<div style="position:fixed; float:right; bottom:0;right:0;">\
-    <img src="https://licensebuttons.net/l/by-nd/4.0/88x31.png" alt="Лицензия">\
-    <p>Лицензия <a href="http://creativecommons.org/licenses/by-nd/4.0/" target="_blank">CC BY-ND</a></p>\
-</div>';
+    var license = '<div style="position:fixed; float:right; bottom:0;right:10px;">\
+    <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/" title="Creative Commons Attribution-NoDerivs 4.0 International license" target="_blank">\
+    <img src="https://licensebuttons.net/l/by-nd/4.0/88x31.png" alt="Лицензия"></a>';
     var content_html = document.getElementById("content").innerHTML;
     document.getElementById("content").innerHTML = content_html + license;
     })();
